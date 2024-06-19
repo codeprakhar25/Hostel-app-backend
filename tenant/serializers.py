36b912b5666f377f17fa7ef7b6a317a1f63399da
codeprakhar25 <prakharkhatri123@gmail.com>
2024-06-20 04:02:49 +0530

    fix: minor

--- tenant/serializers.py
+++ tenant/serializers.py
@@ -2,10 +2,6 @@
 from rest_framework import serializers
 from .models import Tenant, Rent, Attachment
 
-class TenantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tenant
-        fields = '__all__'
 
 class RentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,4 +11,11 @@ class RentSerializer(serializers.ModelSerializer):
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attachment
+        fields = '__all__'
+
+class TenantSerializer(serializers.ModelSerializer):
+    rents = RentSerializer(many=True, read_only=True)
+    attachments = AttachmentSerializer(many=True, read_only=True)
+    class Meta:
+        model = Tenant
         fields = '__all__'
